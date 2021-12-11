97e6570b11652711970ff90084cb43ece4e5c6a2
walker95279527 <1006324179@qq.com>
2021-12-11 23:08:46 +0800

    22

---
+++ gshop-client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+export default {
+  mounted() {
+    //this.$store.dispatch('getAddress')
+    this.getAddress()
+    this.getUserInfo()
+  },
+
+  methods: {
+    ...mapActions(['getAddress','getUserInfo'])
+  },
+
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
